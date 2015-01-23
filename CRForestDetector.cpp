5dcb9f50c94c9f3b3eff9f7e74da3aaa7d7c6368
Craig Henderson <hello@craighenderson.co.uk>
2015-01-23 17:10:17 +0000

    separated regression and hough voting

--- CRForestDetector.cpp
+++ CRForestDetector.cpp
@@ -20,15 +20,64 @@ void CRForestDetector::detectColor(
 	// extract features
 	vector<IplImage*> features;
 	CRPatch::extractFeatureChannels(img, features);
-    accumulate_votes({img->width, img->height}, imgDetect, features, ratios);
+    accumulate_votes({img->width, img->height}, features, ratios, imgDetect);
 }
 
+std::vector<std::vector<std::vector<LeafNode const *>>>
+CRForestDetector::run_regression(
+    CvSize                 const &size,
+    std::vector<IplImage*> const &features)
+{
+    cdmh::timer t("CRForestDetector::run_regression");
+
+    std::vector<std::vector<std::vector<LeafNode const *>>> results;
+
+	// get pointers to feature channels
+	int stepImg = 0;
+	uchar** ptFCh     = new uchar*[features.size()];
+	uchar** ptFCh_row = new uchar*[features.size()];
+	for (unsigned int c=0; c<features.size(); ++c)
+		cvGetRawData(features[c], (uchar**)&(ptFCh[c]), &stepImg);
+	stepImg /= sizeof(ptFCh[0][0]);
+
+	int const xoffset = width/2;
+	int const yoffset = height/2;
+
+    results.resize(size.height-height);
+	for (int y=0, cy=yoffset; y<size.height-height; ++y, ++cy)
+    {
+        results[y].resize(size.width-width);
+
+		// Get start of row
+		for (unsigned int c=0; c<features.size(); ++c)
+			ptFCh_row[c] = &ptFCh[c][0];
+		
+		for (int x=0, cx=xoffset; x<size.width-width; ++x, ++cx)
+        {
+			// regression for a single patch
+			crForest_.regression(results[y][x], ptFCh_row, stepImg);
+
+			// increase pointer - x
+			for (size_t c=0; c<features.size(); ++c)
+				++ptFCh_row[c];
+		} // end for x
+
+		// increase pointer - y
+		for(unsigned int c=0; c<features.size(); ++c)
+			ptFCh[c] += stepImg;
+
+	} // end for y 	
+
+	delete[] ptFCh;
+	delete[] ptFCh_row;
+    return results;
+}
 
 void CRForestDetector::accumulate_votes(
     CvSize                 const &size,
-    vector<IplImage*>            &imgDetect,
     std::vector<IplImage*> const &features,
-    std::vector<float>     const &ratios)
+    std::vector<float>     const &ratios,
+    std::vector<IplImage*>       &imgDetect)
 {
     cdmh::timer t("CRForestDetector::accumulate_votes");
 
@@ -69,9 +118,110 @@ void CRForestDetector::accumulate_votes(
 			// regression for a single patch
 			vector<const LeafNode*> result;
 			crForest_.regression(result, ptFCh_row, stepImg);
-			
+
 			// vote for all trees (leafs) 
-			for(vector<const LeafNode*>::const_iterator itL = result.begin(); itL!=result.end(); ++itL) {
+			for (vector<const LeafNode*>::const_iterator itL = result.begin(); itL!=result.end(); ++itL)
+            {
+
+				// To speed up the voting, one can vote only for patches 
+			    // with a probability for foreground > 0.5
+                // !!! CH This was commented out in the original code, with no
+                //        indication why. It reduces processing from 4m to 20s
+                //        on a debug build, so worth having, and produces a
+                //        good result. I haven't compared the accuracy fully
+                //        yet, though
+				if((*itL)->pfg>0.5) {
+
+					// voting weight for leaf 
+					float w = (*itL)->pfg / float( (*itL)->vCenter.size() * result.size() );
+
+					// vote for all points stored in the leaf
+					for(vector<vector<CvPoint> >::const_iterator it = (*itL)->vCenter.begin(); it!=(*itL)->vCenter.end(); ++it) {
+
+						for(int c=0; c<(int)imgDetect.size(); ++c) {
+						  int const x = int(cx - (*it)[0].x * ratios[c] + 0.5);
+						  int const y = cy-(*it)[0].y;
+						  if(y>=0 && y<imgDetect[c]->height && x>=0 && x<imgDetect[c]->width) {
+						    *(ptDet[c]+x+y*stepDet) += w;
+						  }
+						}
+					}
+
+				} // end if
+
+			}
+
+			// increase pointer - x
+			for(unsigned int c=0; c<features.size(); ++c)
+				++ptFCh_row[c];
+
+		} // end for x
+
+		// increase pointer - y
+		for(unsigned int c=0; c<features.size(); ++c)
+			ptFCh[c] += stepImg;
+
+	} // end for y 	
+
+	// smooth result image
+	for(int c=0; c<(int)imgDetect.size(); ++c)
+		cvSmooth( imgDetect[c], imgDetect[c], CV_GAUSSIAN, 3);
+	
+	delete[] ptFCh;
+	delete[] ptFCh_row;
+	delete[] ptDet;
+
+}
+
+void CRForestDetector::accumulate_votes(
+    CvSize                                                  const &size,
+    std::vector<IplImage*>                                  const &features,
+    std::vector<float>                                      const &ratios,
+    std::vector<std::vector<std::vector<LeafNode const *>>> const &regression,
+    std::vector<IplImage*>                                        &imgDetect)
+{
+    cdmh::timer t("CRForestDetector::accumulate_votes");
+
+	// reset output image
+	for(int c=0; c<(int)imgDetect.size(); ++c)
+		cvSetZero( imgDetect[c] );
+
+	// get pointers to feature channels
+	int stepImg = 0;
+	uchar** ptFCh     = new uchar*[features.size()];
+	uchar** ptFCh_row = new uchar*[features.size()];
+	for(unsigned int c=0; c<features.size(); ++c) {
+		cvGetRawData( features[c], (uchar**)&(ptFCh[c]), &stepImg);
+	}
+	stepImg /= sizeof(ptFCh[0][0]);
+
+	// get pointer to output image
+	int stepDet = 0;
+	float** ptDet = new float*[imgDetect.size()];
+	for(unsigned int c=0; c<imgDetect.size(); ++c)
+		cvGetRawData( imgDetect[c], (uchar**)&(ptDet[c]), &stepDet);
+	stepDet /= sizeof(ptDet[0][0]);
+
+	int xoffset = width/2;
+	int yoffset = height/2;
+	
+	int x, y, cx, cy; // x,y top left; cx,cy center of patch
+	cy = yoffset; 
+
+	for(y=0; y<size.height-height; ++y, ++cy) {
+		// Get start of row
+		for(unsigned int c=0; c<features.size(); ++c)
+			ptFCh_row[c] = &ptFCh[c][0];
+		cx = xoffset; 
+		
+		for(x=0; x<size.width-width; ++x, ++cx) {					
+
+			// regression for a single patch
+			vector<const LeafNode*> const &result = regression[y][x];
+
+			// vote for all trees (leafs) 
+			for (vector<const LeafNode*>::const_iterator itL = result.begin(); itL!=result.end(); ++itL)
+            {
 
 				// To speed up the voting, one can vote only for patches 
 			    // with a probability for foreground > 0.5
--- CRForestDetector.h
+++ CRForestDetector.h
@@ -20,15 +20,24 @@ public:
 	size_t GetNumCenter() const {return crForest_.GetNumCenter();}
 
     void accumulate_votes(CvSize                 const &size,
-                          std::vector<IplImage*>       &imgDetect,
                           std::vector<IplImage*> const &features,
-                          std::vector<float>     const &ratios);
+                          std::vector<float>     const &ratios,
+                          std::vector<IplImage*>       &imgDetect);
+
+    void accumulate_votes(CvSize                                                  const &size,
+                          std::vector<IplImage*>                                  const &features,
+                          std::vector<float>                                      const &ratios,
+                          std::vector<std::vector<std::vector<LeafNode const *>>> const &regression,
+                          std::vector<IplImage*>                                        &imgDetect);
+
+    std::vector<std::vector<std::vector<LeafNode const *>>>
+    run_regression(CvSize const &size, std::vector<IplImage*> const &features);
 
 private:
     CRForestDetector &operator=(CRForestDetector const &) = delete;
 	void detectColor(IplImage *img, std::vector<IplImage*>& imgDetect, std::vector<float> const &ratios);
 
 	CRForest const &crForest_;
-	int width;
-	int height;
+	int const width;
+	int const height;
 };
